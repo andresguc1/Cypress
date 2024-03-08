f6870ff1ce27877ec8c277af08fb8135f0783dba
Andresguc <andresguc@gmail.com>
2024-03-08 13:49:32 -0500

    add A/B testing test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,13 @@
-describe('The Home page should be load correctly', () => {
-  it('the header display the correct messages', () => {
-    cy.visit('https://the-internet.herokuapp.com/')
-    cy.get(".heading").should("exist").contains("Welcome to the-internet")
-    cy.get("h2").should("exist").contains("Available Examples")
-  })
-})
+describe("The Home page should be load correctly", () => {
+  it("the header display the correct tittles", () => {
+    cy.visit("https://the-internet.herokuapp.com/");
+    cy.get(".heading").should("exist").contains("Welcome to the-internet");
+    cy.get("h2").should("exist").contains("Available Examples");
+  });
+
+  it("The A/B testing page are available", () => {
+    cy.visit("https://the-internet.herokuapp.com/");
+    cy.get("a").contains('A/B Testing').click();
+    cy.get('h3').should('exist').contains('A/B Test Variation 1')
+  });
+});
