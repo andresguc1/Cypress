c10fb579f14bad2aef2e4f4dd890e9536c0b9851
Andresguc <andresguc@gmail.com>
2024-03-08 13:54:55 -0500

    add beforeEach command to the test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,12 +1,17 @@
 describe("The Home page should be load correctly", () => {
-  it("the header display the correct tittles", () => {
+  beforeEach(() => {
     cy.visit("https://the-internet.herokuapp.com/");
+  })
+
+
+  it("the header display the correct tittles", () => {
+
     cy.get(".heading").should("exist").contains("Welcome to the-internet");
     cy.get("h2").should("exist").contains("Available Examples");
   });
 
   it("The A/B testing page are available", () => {
-    cy.visit("https://the-internet.herokuapp.com/");
+
     cy.get("a").contains('A/B Testing').click();
     cy.get('h3').should('exist').contains('A/B Test Variation 1')
   });
