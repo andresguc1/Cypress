edeeff9cff25457a23bf91f1aee90cbf6f9ff512
Andresguc <andresguc@gmail.com>
2024-03-12 15:44:09 -0500

    add test validation for entry ad in a modal

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,7 @@
- // Custom command to wait for text to be visible
-  function waitForTextToBeVisible(text) {
-    cy.contains(text, { timeout: 10000 }).should('be.visible');
-  }
+// Custom command to wait for text to be visible
+function waitForTextToBeVisible(text) {
+  cy.contains(text, { timeout: 10000 }).should("be.visible");
+}
 
 describe("The Home page features should be works correctly", () => {
   beforeEach(() => {
@@ -78,86 +78,96 @@ describe("The Home page features should be works correctly", () => {
   it("Should locate Challenging DOM", () => {
     cy.get("a").should("exist").contains("Challenging DOM").click();
     cy.get("h3").contains("Challenging DOM");
-    cy.get('.large-2.columns').find('a.button').eq(0).click()
-    cy.get('div.large-10.columns canvas#canvas').invoke('attr', 'width').then((width) => {
-      const canvasWidth = parseInt(width);
-      // Now you can use the canvasWidth variable as needed in your test
-      // For example, you can assert that the width is equal to a certain value
-      expect(canvasWidth).to.equal(599);
-    });
+    cy.get(".large-2.columns").find("a.button").eq(0).click();
+    cy.get("div.large-10.columns canvas#canvas")
+      .invoke("attr", "width")
+      .then((width) => {
+        const canvasWidth = parseInt(width);
+        // Now you can use the canvasWidth variable as needed in your test
+        // For example, you can assert that the width is equal to a certain value
+        expect(canvasWidth).to.equal(599);
+      });
   });
 
   it("Should the checkbox could be selected", () => {
     cy.get("a").should("exist").contains("Checkboxes").click();
     cy.get("h3").contains("Checkboxes");
-    cy.get('input[type="checkbox"]').eq('0').check();
-    cy.get('input[type="checkbox"]').should('be.checked');
-    cy.get('input[type="checkbox"]').eq('1').uncheck();
-    cy.get('input[type="checkbox"]').eq('1').should('not.be.checked');
+    cy.get('input[type="checkbox"]').eq("0").check();
+    cy.get('input[type="checkbox"]').should("be.checked");
+    cy.get('input[type="checkbox"]').eq("1").uncheck();
+    cy.get('input[type="checkbox"]').eq("1").should("not.be.checked");
   });
 
   it("The Context Menu should be displayed", () => {
     cy.get("a").should("exist").contains("Context Menu").click();
     cy.get("h3").contains("Context Menu");
-    cy.get('#hot-spot').should('is.visible').trigger('contextmenu'); // TODO: is not possible?
+    cy.get("#hot-spot").should("is.visible").trigger("contextmenu"); // TODO: is not possible?
   });
 
   it("Test disappears elements", () => {
     cy.get("a").should("exist").contains("Disappearing Elements").click();
     cy.get("h3").contains("Disappearing Elements");
-    cy.contains('li', 'About').click();
-    cy.get('h1').should('has.text', 'Not Found');
-    cy.go('back')
+    cy.contains("li", "About").click();
+    cy.get("h1").should("has.text", "Not Found");
+    cy.go("back");
     cy.get("h3").contains("Disappearing Elements");
-    cy.contains('li', 'Contact Us').click();
-    cy.get('h1').should('has.text', 'Not Found');
-    cy.go('back')
+    cy.contains("li", "Contact Us").click();
+    cy.get("h1").should("has.text", "Not Found");
+    cy.go("back");
     cy.get("h3").contains("Disappearing Elements");
-    cy.contains('li', 'Portfolio').click();
-    cy.get('h1').should('has.text', 'Not Found');
-    cy.go('back')
+    cy.contains("li", "Portfolio").click();
+    cy.get("h1").should("has.text", "Not Found");
+    cy.go("back");
     cy.get("h3").contains("Disappearing Elements");
   });
 
   it("Should verify content changes after reload", () => {
     cy.get("a").should("exist").contains("Dynamic Content").click();
     cy.get("h3").contains("Dynamic Content");
-    cy.get(':nth-child(1) > .large-2 > img').should('is.exist')
-    cy.get(':nth-child(4) > .large-2 > img').should('is.exist')
-    cy.get(':nth-child(7) > .large-2 > img').should('is.exist')
-    cy.get(':nth-child(1) > .large-10').invoke('text').then((text) => {
-      cy.log('Text Content:', text);
-    });
-    cy.get(':nth-child(4) > .large-10').invoke('text').then((text) => {
-      cy.log('Text Content:', text);
-    });
-    cy.get(':nth-child(4) > .large-10').invoke('text').then((text) => {
-      cy.log('Text Content:', text);
-    });
+    cy.get(":nth-child(1) > .large-2 > img").should("is.exist");
+    cy.get(":nth-child(4) > .large-2 > img").should("is.exist");
+    cy.get(":nth-child(7) > .large-2 > img").should("is.exist");
+    cy.get(":nth-child(1) > .large-10")
+      .invoke("text")
+      .then((text) => {
+        cy.log("Text Content:", text);
+      });
+    cy.get(":nth-child(4) > .large-10")
+      .invoke("text")
+      .then((text) => {
+        cy.log("Text Content:", text);
+      });
+    cy.get(":nth-child(4) > .large-10")
+      .invoke("text")
+      .then((text) => {
+        cy.log("Text Content:", text);
+      });
   });
 
   it("Validate the dynamic control of the feature", () => {
     cy.get("a").should("exist").contains("Dynamic Controls").click();
     cy.get("h4").contains("Dynamic Controls");
-    cy.get('#checkbox > input').should('is.visible').check()
+    cy.get("#checkbox > input").should("is.visible").check();
 
     // Locate the  element to add Remove/add by its attributes
-    cy.get('.subheader').should('be.visible').contains('Remove/add');
-    cy.get('button[type="button"][onclick="swapCheckbox()"]').as('removeButton');
-    cy.get('@removeButton').click();
-    cy.get('#loading').should('be.visible')
-    cy.get('#message').should('is.visible').contains("It's gone!")
+    cy.get(".subheader").should("be.visible").contains("Remove/add");
+    cy.get('button[type="button"][onclick="swapCheckbox()"]').as(
+      "removeButton"
+    );
+    cy.get("@removeButton").click();
+    cy.get("#loading").should("be.visible");
+    cy.get("#message").should("is.visible").contains("It's gone!");
 
     // Locate the  element to add Enable/disable by its attributes
-    cy.get('.subheader').should('be.visible').contains('Enable/disable');
-    cy.get('button[type="button"][onclick="swapInput()"]').as('enableButton');
-    cy.get('@enableButton').click();
-    cy.get('#message').should('is.visible').contains("It's enabled!")
-    cy.get('input').type('This is a Cypress test')
+    cy.get(".subheader").should("be.visible").contains("Enable/disable");
+    cy.get('button[type="button"][onclick="swapInput()"]').as("enableButton");
+    cy.get("@enableButton").click();
+    cy.get("#message").should("is.visible").contains("It's enabled!");
+    cy.get("input").type("This is a Cypress test");
 
     // Reverse changes
-    cy.get('@removeButton').click();
-    cy.get('#message').should('is.visible').contains("It's back")
+    cy.get("@removeButton").click();
+    cy.get("#message").should("is.visible").contains("It's back");
   });
 
   it("Validate the Dynamic Loading", () => {
@@ -165,17 +175,47 @@ describe("The Home page features should be works correctly", () => {
     cy.get("a").should("exist").contains("Dynamic Loading").click();
     cy.get("h3").contains("Dynamically Loaded Page Elements");
     cy.get('[href="/dynamic_loading/1"]').click();
-    cy.get('h4').contains('Example 1: Element on page that is hidden').should('be.visible');
-    cy.get('button').click();
-    waitForTextToBeVisible('Hello World!');
-  
+    cy.get("h4")
+      .contains("Example 1: Element on page that is hidden")
+      .should("be.visible");
+    cy.get("button").click();
+    waitForTextToBeVisible("Hello World!");
+
     // Go back to the previous page to validate Example 2
-    cy.go('back');
-  
+    cy.go("back");
+
     // Example 2: Element is not on the page and gets added in
     cy.get('[href="/dynamic_loading/2"]').click();
-    cy.get('h4').contains('Example 2: Element rendered after the fact').should('be.visible');
-    cy.get('button').click();
-    waitForTextToBeVisible('Hello World!');
+    cy.get("h4")
+      .contains("Example 2: Element rendered after the fact")
+      .should("be.visible");
+    cy.get("button").click();
+    waitForTextToBeVisible("Hello World!");
+  });
+
+  it("Test the Entry Ad feature", () => {
+    cy.get("a").should("exist").contains("Entry Ad").click();
+
+    // Assert that the modal is now visible
+    cy.get(".modal").should("be.visible");
+
+    // Assert the title of the modal
+    cy.get(".modal-title h3").should('be.visible').contains("This is a modal window");
+
+    // Assert the content of the modal body
+    cy.get(".modal-body p").contains(
+      "It's commonly used to encourage a user to take an action"
+    );
+
+    // Close the modal (e.g., by clicking the Close button)
+    // Replace '.modal-footer p' with the appropriate selector for the Close button
+    cy.get(".modal-footer p").click();
+
+    // Assert that the modal is closed
+    cy.get(".modal").should("not.be.visible");
+
+    // Re-enable the modal
+    cy.get("a").should("exist").contains("click here").click();
+    cy.get(".modal-title h3").should('be.visible').contains("This is a modal window");
   });
 });
